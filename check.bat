bc222630e93d5ce8079fdcfceb11215d053f4ea2
Michael FIG <michael@liveblockauctions.com>
2019-01-04 12:21:13 -0600

    Implement a combinatorial test suite.

---
+++ check.bat
@@ -0,0 +1,19 @@
+:; thisdir=`dirname "$0"`; status=0; # set -x
+:; langs=${1+"$@"}
+:; test -n "$langs" || langs=`cd "$thisdir"/lang && echo *`
+:; mkdir -p "$thisdir/checkout"
+:; for lang in $langs; do
+:;  for emitter in "$thisdir"/lib/emit-*.mjs; do
+:;    target=`echo "$emitter" | sed -e 's/.*\/emit-//; s/\.mjs$//'`
+:;    sfx=`cat "$thisdir/lang/$target/.suffix"`
+:;    out="$thisdir/checkout/${lang}2$target.$sfx"
+:;    echo "==== Run $emitter in $lang to generate $out";
+:;    "$thisdir/lang/$lang/jesspipe.bat" "$emitter" -- "$thisdir"/lib/*.mjs > "$out" || { status=$?; break; }
+:;    diff -u "$thisdir/lang/$target/jessica.$sfx" "$out" || { status=$?; break; }
+:;  done
+:; done
+:; exit $status
+@echo off
+set thisdir=%~dp0
+set arguments=%*
+echo Not implemented for Windows yet.
--- lang/c/jesspipe.c
+++ lang/c/jesspipe.c
@@ -5,7 +5,7 @@
 int
 main(int argc, char **argv) {
     JSEState js = calloc(sizeof(*js), 1);
-    printf("Alloced %p\n", js);
+    fprintf(stderr, "FIXME: Would do something, other than alloc %p\n", js);
     free(js);
     return 0;
 }
