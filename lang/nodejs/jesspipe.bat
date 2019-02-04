e68c8e057f64906ded83d8d801f0b6095110cba3
Michael FIG <michael@liveblockauctions.com>
2019-02-03 20:20:49 -0600

    More work to pass tests.

--- lang/nodejs/jesspipe.bat
+++ lang/nodejs/jesspipe.bat
@@ -1,9 +1,9 @@
 :; thisdir=`dirname "$0"`;
 :; test -d "$thisdir/node_modules/esm" && test -d "$thisdir/node_modules/ses" || { cd "$thisdir" && npm install; }
-:; parentdir=`cd "$thisdir" && pwd`
+:; athisdir=`cd "$thisdir" && pwd`
 :; curdir=`pwd`
-:; reldir=`echo "$parentdir" | sed -e "s!^$curdir/!!"`
-:; NODE_PATH="$thisdir/node_modules:$NODE_PATH" exec node --require esm "$reldir/jesspipe.mjs" ${1+"$@"}
+:; reldir=`echo "$athisdir" | sed -e "s!^$curdir/!!;"' s!\(.\)$!\1/!'`
+:; NODE_PATH="$thisdir/node_modules:$NODE_PATH" exec node --require esm "${reldir}jesspipe.mjs" ${1+"$@"}
 :; echo 1>&2 "Failed!";exit 1
 @echo off
 set thisdir=%~dp0
