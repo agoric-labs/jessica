b57f4375b8f0b62afe20eddaa49fda05a5101988
Michael FIG <michael@fig.org>
2019-02-28 22:32:07 -0600

    Test some more primitives.

--- lang/nodejs/bond.test.ts
+++ lang/nodejs/bond.test.ts
@@ -5,10 +5,13 @@ import './globalEnv.mjs';
 
 import makeBond from '../../lib/bond.mjs';
 
-test('bond() primitives', () => {
+test('bond(primitives)', () => {
     const bond = makeBond((that, index) => that[index], (that, method, args) => method.apply(that, args));
     expect(bond('foo')).toBe('foo');
     expect(bond(123)).toBe(123);
     expect(bond(0.995)).toBe(0.995);
+    expect(bond(false)).toBe(false);
+    expect(bond(undefined)).toBe(undefined);
+    expect(bond(null)).toBe(null);
     expect(bond((arg) => arg)(123)).toBe(123);
 });
