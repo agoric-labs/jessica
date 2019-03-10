8b90655de9f912ec53f219beb2dd7c9fe34810e9
Michael FIG <michael@fig.org>
2019-03-10 00:36:29 -0600

    Prevent import recursion.

--- lib/importer.mjs.ts
+++ lib/importer.mjs.ts
@@ -2,16 +2,31 @@ import tagString from './tag-string.mjs';
 
 function makeImporter(readInput: (file: string) => string, languageTag: IParserTag<any[]>) {
     const importCache = makeMap<string, any>();
+
+    const IMPORT_RECURSION = {
+        toString: () => 'IMPORT_RECURSION',
+    };
+
     function importer(file: string, evaluator: (ast: any[]) => any) {
         let val: any;
         if (!importCache.has(file)) {
-            const src = readInput(file);
-            const strParser = tagString(languageTag, file);
-            const ast = strParser`${src}`;
-            val = evaluator(ast);
+            try {
+                importCache.set(file, IMPORT_RECURSION);
+                const src = readInput(file);
+                const strParser = tagString(languageTag, file);
+                const ast = strParser`${src}`;
+                val = evaluator(ast);
+            } catch (e) {
+                // Clean up our recursion tag on error.
+                importCache.delete(file);
+                throw e;
+            }
             importCache.set(file, val);
         } else {
             val = importCache.get(file);
+            if (val === IMPORT_RECURSION) {
+                slog.error`Import recursion while resolving ${{file}}`;
+            }
         }
         return val;
     }
