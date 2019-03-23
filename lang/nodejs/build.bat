b4785d5d04e2c228ee8df85edded507b7fa9b70b
Michael FIG <michael@fig.org>
2019-03-22 22:56:28 -0600

    Make build idempotent again.

--- lang/nodejs/build.bat
+++ lang/nodejs/build.bat
@@ -2,7 +2,7 @@
 :; npm install || exit $?
 :; status=0
 :; rm -rf tsout
-:; tsc -p ./tsconfig.app.json --outDir tsout || status=$?
+:; ./node_modules/.bin/tsc -p ./tsconfig.app.json --outDir tsout || status=$?
 :; for f in tsout/lang/nodejs/*.js; do test -f "$f" || continue; mv "$f" `echo "$f" | sed -e 's!^.*/\(.*\.mjs\)\.js$!\1!; s!^.*/\(.*\.js\)$!\1!'`; done
 :; rm -rf tsout
 :; exit $status
