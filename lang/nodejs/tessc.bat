8625b563b327cb83bd47aa48deabb6cff6af76ee
Michael FIG <michael@fig.org>
2019-03-26 19:49:21 -0600

    Fix security audit.

--- lang/nodejs/tessc.bat
+++ lang/nodejs/tessc.bat
@@ -1,6 +1,6 @@
 :; thisdir=`dirname "$0"`;
 :; test -d "$thisdir/node_modules/typescript" || (cd "$thisdir" && npm install 1>&2)
-:; test "$thisdir/tessc.js" -nt "$thisdir/tessc.ts" || (cd "$thisdir" && tsc -p ./tsconfig.tessc.json --outDir .)
+:; test "$thisdir/tessc.js" -nt "$thisdir/tessc.ts" || (cd "$thisdir" && ./node_modules/.bin/tsc -p ./tsconfig.tessc.json --outDir .)
 :; exec node "$thisdir/tessc.js" ${1+"$@"}
 :; echo 1>&2 "Failed!";exit 1
 @echo off
