d75ed405394015a160ce592cd3f0bec8a5545a67
Michael FIG <michael@fig.org>
2019-03-28 23:10:39 -0600

    Update lang/browser/index.html

--- lang/browser/build.bat
+++ lang/browser/build.bat
@@ -1,4 +1,4 @@
 :; cd `dirname "$0"` || exit $?
 :; test -d ./node_modules/parcel-bundler || npm install || exit $?
-:; exec ./node_modules/.bin/parcel build --global=jessica ./jessica.mjs
+:; exec ./node_modules/.bin/parcel build -d . --global=jessica ./jessica.mjs
 :; exit $?
