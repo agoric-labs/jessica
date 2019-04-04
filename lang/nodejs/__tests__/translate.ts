3232409b56728b09904ff21f76cb89b2ec7c8547
Michael FIG <michael@fig.org>
2019-04-04 14:45:38 -0600

    Rewrite more imports/exports.

--- lang/nodejs/__tests__/translate.ts
+++ lang/nodejs/__tests__/translate.ts
@@ -14,15 +14,15 @@ const params: IJessicaResourceParameters = {
 const modParams: IJessicaResourceParameters = {
     ...params,
     targetType: 'module',
-}
+};
 
 test('sanity', async () => {
-    const translatedText = `\$h_define(
+    const translatedText = `$h_define(
 [],
 () => {
-const \$h_exports = {};
-\$h_exports.default = immunize(() => 'hello world');
-return \$h_exports;
+const $h_exports = {};
+$h_exports.default = immunize(() => 'hello world');
+return $h_exports;
 });`;
     expect(await translate(`export default immunize(() => 'hello world');`, params)).toEqual({
         ...params,
@@ -34,3 +34,44 @@ return \$h_exports;
         translatedText: module,
     });
 });
+
+test('imports', async () => {
+    const translatedText = `$h_define(
+["./foo"],
+($h_star0) => {
+const $h_exports = {};
+const {default: $i_foo} = $h_star0;
+const foo = immunize($i_foo);
+$h_exports.default = immunize(foo);
+return $h_exports;
+});`;
+    expect(await translate(`import $i_foo from './foo';
+const foo = immunize($i_foo);
+export default immunize(foo);
+`,
+        params)).toEqual({
+            ...params,
+            translatedText,
+        });
+
+    const translatedText2 = `$h_define(
+["./foo"],
+($i_bitzy) => {
+const $h_exports = {};
+const {default: $i_foo} = $i_bitzy;
+const foo = immunize($i_foo), {bat, boz} = immunize($i_bitzy);
+$h_exports.foo = foo;
+$h_exports.bat = bat;
+$h_exports.boz = boz;
+$h_exports.default = immunize(boz);
+return $h_exports;
+});`;
+    expect(await translate(`import $i_foo, * as $i_bitzy from './foo';
+export const foo = immunize($i_foo), {bat, boz} = immunize($i_bitzy);
+export default immunize(boz);
+`,
+        params)).toEqual({
+            ...params,
+            translatedText: translatedText2,
+        });
+    });
--- lib/rewrite-ses.mjs.ts
+++ lib/rewrite-ses.mjs.ts
@@ -5,25 +5,100 @@ const moduleRewriteSES = (moduleAST: any[]) => {
     const EXPORTS = hide('exports');
     const DEFINE = hide('define');
     const imports = makeMap<string, string>();
+    const exportVars = makeSet<string>();
+    let starName: string;
+    let nImport = 0;
     const rewriters: Record<string, Rewriter> = {
+        as(imp: string, sym: string) {
+            if (imp === '*') {
+                starName = sym;
+                return '';
+            }
+            return `${imp}: ${sym}`;
+        },
+        bind(def: Node, expr: Node) {
+            const name = doRewrite(def);
+            return `${name} = ${doRewrite(expr)}`;
+        },
+        def(name: string) {
+            exportVars.add(name);
+            return name;
+        },
         exportDefaultX(val: Node[]) {
             return `${EXPORTS}.default = ${doRewrite(val)};`;
         },
+        exportX(qual: string, binds: Node[]) {
+            exportVars.clear();
+            let bindings = '', comma = '';
+            for (const bind of binds) {
+                bindings += comma + doRewrite(bind);
+                comma = ', ';
+            }
+            let assign = '';
+            for (const vname of exportVars.keys()) {
+                assign += `${EXPORTS}.${vname} = ${vname};\n`;
+            }
+            exportVars.clear();
+            return `${qual} ${bindings};\n${assign}`;
+        },
         import(clause: Node[], fromModule: string) {
             // Save the bindings.
-            const bindings = clause.reduce<string>((prior, cur) => prior + doRewrite(cur), '');
-            imports.set(fromModule, bindings);
-            // We don't do anything until later.
+            starName = undefined;
+            const bindings = doRewrite(clause);
+            if (starName === undefined) {
+                starName = hide(`star${nImport ++}`);
+            }
+            imports.set(fromModule, starName);
+            if (bindings) {
+                return `const {${bindings}} = ${starName};\n`;
+            }
             return '';
+        },
+        importBind(bindings: Node[]) {
+            let bound = '';
+            let comma = '';
+            for (const binding of bindings) {
+                const b = doRewrite(binding);
+                if (b !== '') {
+                    bound += comma + doRewrite(binding);
+                    comma = ', ';
+                }
+            }
+            return bound;
+        },
+        matchArray(es: Node[]) {
+            let arr = '';
+            let comma = '';
+            for (const e of es) {
+                arr += comma + doRewrite(e);
+                comma = ', ';
+            }
+            return `[${arr}]`;
+        },
+        matchProp(kw: string, prop: Node) {
+            const rewrite = doRewrite(prop);
+            if (kw === rewrite) {
+                return kw;
+            }
+            return `${kw}: ${rewrite}`;
+        },
+        matchRecord(es: Node[]) {
+            let rec = '';
+            let comma = '';
+            for (const e of es) {
+                rec += comma + doRewrite(e);
+                comma = ', ';
+            }
+            return `{${rec}}`;
         },
         moduleX(decls: Node[]) {
             const body = decls.reduce<string>((prior, cur) => prior + doRewrite(cur), '');
             const modules: string[] = [];
             let bindings = '';
             let comma = '';
-            for (const [mod, binding] of imports.entries()) {
+            for (const [mod, name] of imports.entries()) {
                 modules.push(mod);
-                bindings += comma + binding;
+                bindings += comma + name;
                 comma = ', ';
             }
             return `${DEFINE}(
@@ -33,6 +108,12 @@ const ${EXPORTS} = {};
 ${body}
 return ${EXPORTS};
 });`;
+        },
+        rest(expr: Node) {
+            return `...${doRewrite(expr)}`;
+        },
+        restObj(expr: Node) {
+            return `...${doRewrite(expr)}`;
         },
     };
 
